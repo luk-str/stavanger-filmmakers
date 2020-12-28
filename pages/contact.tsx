1af2919e3405d1a6e74a96cac75e0ccb9e4ac536
luk-str <lstrociak@gmail.com>
2020-12-28 17:14:59 +0100

    Add layout and contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,18 @@
+import Layout from "../components/Layout";
+import Head from "next/head";
+
+export const Contact: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact | Stavanger Filmmakers</title>
+      </Head>
+      <Layout>
+        <h2>This is the contact info</h2>
+        <p>Read more of the Lorem ipsum dolor sit amet.</p>
+      </Layout>
+    </>
+  );
+};
+
+export default Contact;
