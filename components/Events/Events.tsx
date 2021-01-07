53719afee4c1bf931f2b4107af8f16def70dd193
luk-str <lstrociak@gmail.com>
2021-01-07 19:52:51 +0100

    Add links

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./Events.module.css";
 
 const Events: React.FC = () => {
@@ -5,18 +6,26 @@ const Events: React.FC = () => {
     <>
       <h2> Upcoming Events </h2>
       <ul className={styles.list}>
-        <li>
-          Filmmaking club: Let's make/discuss films together!
-          <span className={styles.date}>06.01.2020 - Sølvberget</span>
-        </li>
-        <li>
-          Filmmaking at Kunst Rom Arbeid!
-          <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
-        </li>
-        <li>
-          Filmmaking club: Let's make/discuss films together!
-          <span className={styles.date}>08.03.2020 - Sølvberget</span>
-        </li>
+        <Link href="/events">
+          <li>
+            Filmmaking club: Let's make/discuss films together!
+            <span className={styles.date}>06.01.2020 - Sølvberget</span>
+          </li>
+        </Link>
+
+        <Link href="/events">
+          <li>
+            Filmmaking at Kunst Rom Arbeid!
+            <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
+          </li>
+        </Link>
+
+        <Link href="/events">
+          <li>
+            Filmmaking club: Let's make/discuss films together!
+            <span className={styles.date}>08.03.2020 - Sølvberget</span>
+          </li>
+        </Link>
       </ul>
     </>
   );
