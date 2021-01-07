df5862cbce86df82c2bcdd8df3a782925ea2495f
luk-str <lstrociak@gmail.com>
2021-01-07 19:53:55 +0100

    Add link to go back

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Head from "next/head";
 import Events from "../components/Events/Events";
 
@@ -7,6 +8,9 @@ export const EventPage: React.FC = () => {
       <Head>
         <title>Events | Stavanger Filmmakers</title>
       </Head>
+      <Link href="/">
+        <a>Go back</a>
+      </Link>
       <Events />
     </>
   );
