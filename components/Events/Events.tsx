c3b3def80e5fc26d064a223e3d33110070f788f2
luk-str <lstrociak@gmail.com>
2021-01-07 19:48:58 +0100

    Add temporary content to display

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -2,9 +2,23 @@ import styles from "./Events.module.css";
 
 const Events: React.FC = () => {
   return (
-    <article>
-      <h1> Events </h1>
-    </article>
+    <>
+      <h2> Upcoming Events </h2>
+      <ul className={styles.list}>
+        <li>
+          Filmmaking club: Let's make/discuss films together!
+          <span className={styles.date}>06.01.2020 - Sølvberget</span>
+        </li>
+        <li>
+          Filmmaking at Kunst Rom Arbeid!
+          <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
+        </li>
+        <li>
+          Filmmaking club: Let's make/discuss films together!
+          <span className={styles.date}>08.03.2020 - Sølvberget</span>
+        </li>
+      </ul>
+    </>
   );
 };
 
