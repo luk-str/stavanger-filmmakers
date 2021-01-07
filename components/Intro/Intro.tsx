dfc2a7576eae01694de17db02c9b110b6882864e
luk-str <lstrociak@gmail.com>
2021-01-07 19:28:19 +0100

    Add basic intro

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import styles from "./Intro.module.css";
 
 const Intro: React.FC = () => {
   return (
     <article>
-      <h1> Intro </h1>
+      <h1> Stavanger Filmmakers Club </h1>
+      <p>
+        This is a quick introduction to the club. Two or three sentences to get
+        people interested.
+        <br />
+        <Link href="/fullstory">
+          <a>Here's the full story.</a>
+        </Link>
+      </p>
     </article>
   );
 };
