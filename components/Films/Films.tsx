98a515c6737c6783248d8e82f32645eeeb3501ce
luk-str <lstrociak@gmail.com>
2021-01-08 20:52:24 +0100

    Start working on film thumbnails

--- components/Films/Films.tsx
+++ components/Films/Films.tsx
@@ -1,10 +1,19 @@
 import styles from "./Films.module.css";
+import filmData from "../../shared/data/films.json";
+import type { FilmItem } from "../../shared/types";
+
+import FilmThumbnail from "./FilmThumbnail";
 
 const Films: React.FC = () => {
+  const films: FilmItem[] = filmData.data;
+
   return (
-    <article>
-      <h1> Films </h1>
-    </article>
+    <>
+      <h2> Films </h2>
+      {films.map((film, index) => (
+        <FilmThumbnail film={film} key={index} />
+      ))}
+    </>
   );
 };
 
