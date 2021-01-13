2fee46d5d3e23f953a62d145833c4b2b264b0b38
luk-str <lstrociak@gmail.com>
2021-01-13 18:03:13 +0100

    Add member import

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,11 +1,28 @@
 import Metadata from "../../components/Metadata";
 import Members from "../../components/Members/Members";
+import { getMembers } from "../../shared/lib/sanity";
+import type { Member } from "../../shared/types";
 
-export const MembersIndex: React.FC = () => (
+type Props = {
+  members: Member[];
+};
+
+export const MembersIndex = ({ members }: Props) => (
   <>
     <Metadata />
-    <Members />
+    <Members members={members} />
   </>
 );
 
 export default MembersIndex;
+
+export async function getStaticProps() {
+  const members = await getMembers();
+
+  return {
+    props: {
+      members,
+    },
+    revalidate: 1,
+  };
+}
