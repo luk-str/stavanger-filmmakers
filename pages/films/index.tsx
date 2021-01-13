642b673b6b58a7dc9662ac96cfdcaa2651a1c895
luk-str <lstrociak@gmail.com>
2021-01-13 18:03:57 +0100

    Add types

--- pages/films/index.tsx
+++ pages/films/index.tsx
@@ -1,6 +1,11 @@
 import Metadata from "../../components/Metadata";
 import Films from "../../components/Films/Films";
 import { getThumbnails } from "../../shared/lib/sanity";
+import type { FilmItem } from "../../shared/types";
+
+type Props = {
+  filmThumbnails: FilmItem[];
+};
 
 export const FilmsIndex = ({ filmThumbnails }) => (
   <>
