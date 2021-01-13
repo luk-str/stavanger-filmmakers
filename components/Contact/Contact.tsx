3e006ea85e9ad36e64f5a7533a1a86faa0a5ed19
luk-str <lstrociak@gmail.com>
2021-01-13 16:25:42 +0100

    Add basic contact info

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -4,6 +4,15 @@ const Contact: React.FC = () => {
   return (
     <article>
       <h2> Contact </h2>
+      <a
+        href="mailto:stavangerfilmmakers@gmail.com"
+        title="E-mail Stavanger Filmmakers Club"
+      >
+        stavangerfilmmakers@gmail.com
+      </a>
+      <br />
+      <br />
+      +4790286691
     </article>
   );
 };
