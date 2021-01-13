79644567d5223b2ddad80b8931f2f2a35a67c673
luk-str <lstrociak@gmail.com>
2021-01-13 17:57:30 +0100

    Add thumbnail import

--- pages/films/index.tsx
+++ pages/films/index.tsx
@@ -1,11 +1,23 @@
 import Metadata from "../../components/Metadata";
 import Films from "../../components/Films/Films";
+import { getThumbnails } from "../../shared/lib/sanity";
 
-export const FilmsIndex: React.FC = () => (
+export const FilmsIndex = ({ filmThumbnails }) => (
   <>
     <Metadata />
-    <Films />
+    <Films filmThumbnails={filmThumbnails} />
   </>
 );
 
 export default FilmsIndex;
+
+export async function getStaticProps() {
+  const filmThumbnails = await getThumbnails();
+
+  return {
+    props: {
+      filmThumbnails,
+    },
+    revalidate: 1,
+  };
+}
