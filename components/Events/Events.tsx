170ba0a3a3f78e6b4d8bd76ed8bd6c1076902074
luk-str <lstrociak@gmail.com>
2021-01-15 16:13:27 +0100

    Add a tags to links

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -7,24 +7,30 @@ const Events: React.FC = () => {
       <h2 className={styles.header}> Upcoming Events </h2>
       <ul className={styles.list}>
         <Link href="/events">
-          <li>
-            Filmmaking club: Let's make/discuss films together!
-            <span className={styles.date}>06.01.2020 - Sølvberget</span>
-          </li>
+          <a>
+            <li>
+              Filmmaking club: Let's make/discuss films together!
+              <span className={styles.date}>06.01.2020 - Sølvberget</span>
+            </li>
+          </a>
         </Link>
 
         <Link href="/events">
-          <li>
-            Filmmaking at Kunst Rom Arbeid!
-            <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
-          </li>
+          <a>
+            <li>
+              Filmmaking at Kunst Rom Arbeid!
+              <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
+            </li>
+          </a>
         </Link>
 
         <Link href="/events">
-          <li>
-            Filmmaking club: Let's make/discuss films together!
-            <span className={styles.date}>08.03.2020 - Sølvberget</span>
-          </li>
+          <a>
+            <li>
+              Filmmaking club: Let's make/discuss films together!
+              <span className={styles.date}>08.03.2020 - Sølvberget</span>
+            </li>
+          </a>
         </Link>
       </ul>
     </section>
