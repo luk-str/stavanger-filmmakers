4f954847a65a5090c868621b899b4ff9a07eba3c
luk-str <lstrociak@gmail.com>
2021-01-15 12:37:18 +0100

    Add basic styling to footer

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -2,18 +2,26 @@ import styles from "./Contact.module.css";
 
 const Contact: React.FC = () => {
   return (
-    <article>
-      <h2> Contact </h2>
-      <a
-        href="mailto:stavangerfilmmakers@gmail.com"
-        title="E-mail Stavanger Filmmakers Club"
-      >
-        stavangerfilmmakers@gmail.com
-      </a>
-      <br />
-      <br />
-      +4790286691
-    </article>
+    <>
+      <p>
+        <a
+          href="mailto:stavangerfilmmakers@gmail.com"
+          title="E-mail Stavanger Filmmakers Club"
+          className={`${styles.link} ${styles.link__email}`}
+        >
+          stavangerfilmmakers@gmail.com
+        </a>
+      </p>
+      <p>
+        <a
+          href="tel:+4790286691"
+          title="Call Leo"
+          className={`${styles.link} ${styles.link__phone}`}
+        >
+          +4790286691
+        </a>
+      </p>
+    </>
   );
 };
 
