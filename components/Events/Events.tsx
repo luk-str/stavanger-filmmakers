2f693f1ba50318ae298eeea6a924533a4551b1ea
luk-str <lstrociak@gmail.com>
2021-02-02 12:00:42 +0100

    Reorder li elements to be direct descendants of ul

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -6,32 +6,32 @@ const Events: React.FC = () => {
     <section className={styles.container} id="events">
       <h2 className={styles.header}> Upcoming Events </h2>
       <ul className={styles.list}>
-        <Link href="/events">
-          <a>
-            <li>
+        <li>
+          <Link href="/events">
+            <a>
               Filmmaking club: Let's make/discuss films together!
               <span className={styles.date}>06.01.2020 - Sølvberget</span>
-            </li>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </li>
 
-        <Link href="/events">
-          <a>
-            <li>
+        <li>
+          <Link href="/events">
+            <a>
               Filmmaking at Kunst Rom Arbeid!
               <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
-            </li>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </li>
 
-        <Link href="/events">
-          <a>
-            <li>
+        <li>
+          <Link href="/events">
+            <a>
               Filmmaking club: Let's make/discuss films together!
               <span className={styles.date}>08.03.2020 - Sølvberget</span>
-            </li>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        </li>
       </ul>
     </section>
   );
