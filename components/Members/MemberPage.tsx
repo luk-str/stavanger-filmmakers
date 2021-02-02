84c17eb758b4b25173acaf64b23c9646ef73e6b0
luk-str <lstrociak@gmail.com>
2021-02-02 20:59:14 +0100

    Add first step in displaying link icons

--- components/Members/MemberPage.tsx
+++ components/Members/MemberPage.tsx
@@ -3,9 +3,46 @@ import BlockContent from "@sanity/block-content-to-react";
 import Link from "next/link";
 import { urlFor } from "../../shared/lib/sanity";
 import type { Member } from "../../shared/types";
-import { FiCornerDownLeft } from "react-icons/fi";
+import {
+  FiCornerDownLeft,
+  FiExternalLink,
+  FiFacebook,
+  FiInstagram,
+  FiYoutube,
+} from "react-icons/fi";
+import { SiImdb, SiLinkedin, SiSpotify, SiTwitter } from "react-icons/si";
 
 const MemberPage = ({ member }: { member: Member }) => {
+  function getIconForLink(websiteName: string): JSX.Element {
+    let icon = <FiExternalLink />;
+
+    switch (websiteName) {
+      case "imdb":
+        icon = <SiImdb />;
+        break;
+      case "youtube":
+        icon = <FiYoutube />;
+        break;
+      case "spotify":
+        icon = <SiSpotify />;
+        break;
+      case "linkedin":
+        icon = <SiLinkedin />;
+        break;
+      case "instagram":
+        icon = <FiInstagram />;
+        break;
+      case "facebook":
+        icon = <FiFacebook />;
+        break;
+      case "twitter":
+        icon = <SiTwitter />;
+        break;
+    }
+
+    return icon;
+  }
+
   return (
     <main>
       <Link href="/#members">
@@ -30,6 +67,21 @@ const MemberPage = ({ member }: { member: Member }) => {
         <section className={styles.bio}>
           <BlockContent blocks={member.bio} />
         </section>
+
+        {/* <ul className={styles.links__container}>
+          {member.links.map((link, index) => (
+            <li key={index} className={styles.links__item}>
+              <a
+                href={link.url}
+                title={`Link to ${member.name}'s ${link.websiteName}. Opens in new tab`}
+                rel="noreferrer noopener"
+                target="blank"
+              >
+                {getIconForLink(link.websiteName)}
+              </a>
+            </li>
+          ))}
+        </ul> */}
       </article>
     </main>
   );
