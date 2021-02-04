eaa3b1448347e1183d4e66460bc21428b96c3588
luk-str <lstrociak@gmail.com>
2021-02-04 20:50:21 +0100

    Add display for fetched events

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -1,38 +1,50 @@
 import Link from "next/link";
+import { Event } from "../../shared/types";
 import styles from "./Events.module.css";
 
-const Events: React.FC = () => {
+type Props = {
+  events: Event[];
+};
+
+const Events: React.FC<Props> = ({ events }) => {
+  function formatDate(dateString: string): string {
+    const date = new Date(dateString);
+
+    const [day, month, year] = date.toLocaleDateString().split("/");
+    const [hours, minutes] = date.toLocaleTimeString().split(":");
+
+    return `${day}.${month}.${year}, ${hours}:${minutes}`;
+  }
+
   return (
     <section className={styles.container} id="events">
       <h2 className={styles.header}> Upcoming Events </h2>
-      <ul className={styles.list}>
-        <li>
-          <Link href="/events">
-            <a>
-              Filmmaking club: Let's make/discuss films together!
-              <span className={styles.date}>06.01.2020 - Sølvberget</span>
-            </a>
-          </Link>
-        </li>
 
-        <li>
-          <Link href="/events">
-            <a>
-              Filmmaking at Kunst Rom Arbeid!
-              <span className={styles.date}>12.02.2020 - Kunst Rom Arbeid</span>
-            </a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/events">
-            <a>
-              Filmmaking club: Let's make/discuss films together!
-              <span className={styles.date}>08.03.2020 - Sølvberget</span>
-            </a>
-          </Link>
-        </li>
-      </ul>
+      {events.length > 0 ? (
+        <ul className={styles.list}>
+          {events.map((event) => (
+            <li key={event.date}>
+              <a
+                href={event.link}
+                title={`External website for event ${event.title}. Opens in new tab`}
+                rel="noopener norefferer"
+                target="_blank"
+              >
+                {event.title}
+                <span className={styles.date}>
+                  {formatDate(event.date)} - {event.place}
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.noEvents}>
+          There are currently no upcoming events 😭
+          <br />
+          Please check back later!
+        </p>
+      )}
     </section>
   );
 };
