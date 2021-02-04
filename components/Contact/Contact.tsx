466348aef57f5099e23e0f67992f89e1784a07e8
luk-str <lstrociak@gmail.com>
2021-02-04 18:05:36 +0100

    Add developer link

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -7,39 +7,56 @@ const Contact = ({ contactInfo }: { contactInfo: ContactInfo }) => {
     <footer>
       <h4 className={styles.header}>{contactInfo.header}</h4>
 
-      <p className={styles.link__container}>
-        <a
-          href="https://www.facebook.com/groups/StavangerFilmmakers"
-          title="Stavanger Filmmakers Club Facebook Group"
-          className={`${styles.link} ${styles.link__email}`}
-          target="blank"
-          rel="noreferrer noopener"
-        >
-          <FiFacebook className={styles.icon} />
-          facebook group
-        </a>
-      </p>
-      <p className={styles.link__container}>
-        <a
-          href={`mailto:${contactInfo.mail}`}
-          title="E-mail Stavanger Filmmakers Club"
-          className={`${styles.link} ${styles.link__email}`}
-        >
-          <FiMail className={styles.icon} />
-          {contactInfo.mail}
-        </a>
-      </p>
+      <article className={styles.main__container}>
+        <section>
+          <p className={styles.link__container}>
+            <a
+              href="https://www.facebook.com/groups/StavangerFilmmakers"
+              title="Stavanger Filmmakers Club Facebook Group"
+              className={`${styles.link} ${styles.link__email}`}
+              target="blank"
+              rel="noreferrer noopener"
+            >
+              <FiFacebook className={styles.icon} />
+              facebook group
+            </a>
+          </p>
+          <p className={styles.link__container}>
+            <a
+              href={`mailto:${contactInfo.mail}`}
+              title="E-mail Stavanger Filmmakers Club"
+              className={`${styles.link} ${styles.link__email}`}
+            >
+              <FiMail className={styles.icon} />
+              {contactInfo.mail}
+            </a>
+          </p>
 
-      <p className={styles.link__container}>
-        <a
-          href={`tel:${contactInfo.phone}`}
-          title="Call Stavanger Filmmmakers Club"
-          className={`${styles.link} ${styles.link__phone}`}
-        >
-          <FiPhone className={styles.icon} />
-          {contactInfo.phone}
-        </a>
-      </p>
+          <p className={styles.link__container}>
+            <a
+              href={`tel:${contactInfo.phone}`}
+              title="Call Stavanger Filmmmakers Club"
+              className={`${styles.link} ${styles.link__phone}`}
+            >
+              <FiPhone className={styles.icon} />
+              {contactInfo.phone}
+            </a>
+          </p>
+        </section>
+        <section className={styles.developer}>
+          <p>
+            Code and design by{" "}
+            <a
+              href="https://lukstr.dev"
+              title="Homepage of this website's developer. Opens in new tab."
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Lukas Strociak
+            </a>
+          </p>
+        </section>
+      </article>
     </footer>
   );
 };
