12b36cf19d9115b24c0d46ba8237a35530ad7a8b
luk-str <lstrociak@gmail.com>
2021-02-06 14:10:08 +0100

    Add open graph metadata

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -3,9 +3,29 @@ import Head from "next/head";
 const Metadata: React.FC = () => {
   return (
     <Head>
-      <title>Stavanger Filmmakers</title>
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="description" content="website description" />
+      <title>Stavanger Filmmakers Club</title>
+
+      <meta
+        name="description"
+        content="This is the place for all film lovers, filmmakers and film buffs from the Stavanger area. We meet to discuss, learn from one another and experiment. Interested in filmmaking? Join us now!"
+      />
+      <meta name="author" content="https://luk-str.dev" />
+
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:title" content="Stavanger Filmmakers Club" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="This is the place for all film lovers, filmmakers and film buffs from the Stavanger area. We meet to discuss, learn from one another and experiment. Interested in filmmaking? Join us now!"
+      />
+      <meta
+        property="og:image"
+        content="https://stavangerfilmmakers.com/img/og_image.jpeg"
+      />
+      <meta
+        property="og:image:alt"
+        content="Preview of Stavanger Filmmakers Club's film productions"
+      />
 
       <link
         rel="preload"
