d9b5e46f08c193820af59288537e1a523754cd75
luk-str <lstrociak@gmail.com>
2021-02-06 13:10:04 +0100

    Add presentable members page

--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -1,16 +1,22 @@
 import Metadata from "../../components/Metadata";
 import Members from "../../components/Members/Members";
-import { getMembers } from "../../shared/lib/sanity";
+import { getContactInfo, getMembers } from "../../shared/lib/sanity";
 import type { Member } from "../../shared/types";
+import Contact from "../../components/Contact/Contact";
+import { InferGetStaticPropsType } from "next";
 
-type Props = {
-  members: Member[];
-};
-
-export const MembersIndex = ({ members }: Props) => (
+export const MembersIndex = ({
+  members,
+  contactInfo,
+}: InferGetStaticPropsType<typeof getStaticProps>) => (
   <>
     <Metadata />
-    <Members members={members} />
+
+    <main className="fullHeight">
+      <Members members={members} />
+    </main>
+
+    <Contact contactInfo={contactInfo} />
   </>
 );
 
@@ -18,10 +24,12 @@ export default MembersIndex;
 
 export async function getStaticProps() {
   const members = await getMembers();
+  const contactInfo = await getContactInfo();
 
   return {
     props: {
       members,
+      contactInfo,
     },
     revalidate: 1,
   };
